36d2d2db67e518bdbc01378c1110f6c4bf8ed041
KyDenZ <wk_kydenz@live.fr>
2017-05-29 22:52:17 +0200

    ADDED side menu

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $params = [
     "get"  => $getParams,
     "post" => $postParams
 ];
+$page = (isset($_GET['c']) ? ucfirst($_GET['c']) : "My_phpmyAdmin");
 
 ?>
 
@@ -20,10 +21,12 @@ $params = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title><?php echo $page ?></title>
 
     <!-- Bootstrap -->
     <link href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link href="./assets/css/simple-sidebar.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -33,6 +36,48 @@ $params = [
     <![endif]-->
   </head>
   <body>
+      <div id="wrapper" class="toggled">
+
+       <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        Start Bootstrap
+                    </a>
+                </li>
+                <li>
+                    <a href="#">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#">Shortcuts</a>
+                </li>
+                <li>
+                    <a href="#">Overview</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+             <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+            <a class="navbar-brand" href="#">Data API</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <!--                <li class="active">Results for -->
+                <?php //echo count($results); ?><!-- tweets</li>-->
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <!--                <li><a href="../navbar/">Default</a></li>-->
+                <!--                <li><a href="../navbar-static-top/">Static top</a></li>-->
+                <!--                <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>-->
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
 
 <?php
 
@@ -48,9 +93,19 @@ if (class_exists($class, true)) {
 }
 
 ?>
+   </div>
+    <!-- /#wrapper -->
+
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="./vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
   </body>
 </html>
