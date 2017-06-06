32e63a80cd1b1316a3ef25f455d59ccc1db961a1
KyDenZ <wk_kydenz@live.fr>
2017-06-06 18:53:23 +0200

    FIX

--- app/Controllers/TablesController.php
+++ app/Controllers/TablesController.php
@@ -7,24 +7,8 @@ use Lib\Bdd;
 
 class TablesController extends Controller
 {
-    public $arrayBdd;
-
     public function index()
     {
-        $dataBase = new DataBase();
-        $allBdd = $dataBase->getDatabases();
-        $rsltBdd = [];
-        foreach ($allBdd as $bdd) {
-            $bdd = $bdd[0];
-            $allTables = $dataBase->getTables($bdd);
-            foreach ($allTables as $table) {
-                $table = $table[0];
-                $rsltBdd[$bdd][] = $table;
-            }
-        }
-        $this->arrayBdd = $rsltBdd;
+       echo $_GET["table"];
     }
 }
-
-$sideMenu = new SidemenuController();
-$sideMenu->index();
